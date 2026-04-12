db4ab5e2762b2f88775bcfd7703e50892f23b8fc
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-04-12 11:29:53 +0200

    script

--- .github/scripts/install_llvm_ubuntu.sh
+++ .github/scripts/install_llvm_ubuntu.sh
@@ -7,8 +7,12 @@ llvm_sh=$(mktemp)
 
 wget https://apt.llvm.org/llvm.sh -O "$llvm_sh"
 chmod +x "$llvm_sh"
-"$llvm_sh" "$v" all
+"$llvm_sh" "$v"
 for bin in "${bins[@]}"; do
+    if ! command -v "$bin-$v" &>/dev/null; then
+        echo "Warning: $bin-$v not found" 1>&2
+        continue
+    fi
     ln -fs "$(which "$bin-$v")" "/usr/bin/$bin"
 done
 
